673665e076058ef88d17f0e733fc0a654ca8dd81
Idjarurir <83410327+Idjarurir@users.noreply.github.com>
2024-06-30 22:42:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const config = require("./config");
 
 const app = express();
 
-app.use(express.json());//na saida 
+app.use(express.json());
 app.use(cors());
-app.use(bodyparser.json());//na entrada 
+app.use(bodyparser.json());
 
 var conString = config.urlConnection;
 
@@ -27,8 +27,8 @@ client.connect((err) => {
 });
 
 app.get("/", (req, res) => {
-  console.log("Response ok.");//vai para powershell
-  res.send("Ok – Servidor disponível.");//vai p browser
+  console.log("Response ok.");
+  res.send("Ok – Servidor disponível.");
 });
 
 app.get("/usuarios", (req, res) => {
@@ -45,17 +45,16 @@ app.get("/usuarios", (req, res) => {
   }
 });
 
-app.get("/usuarios/:id", (req, res) => {
+app.get("/usuarios/:email", (req, res) => {
   try {
-    console.log("Rota: usuarios/" + req.params.id);
+    console.log("Rota: usuarios/" + req.params.email);
     client.query(
-      "SELECT * FROM Usuarios WHERE id = $1", [req.params.id],
+      "SELECT * FROM Usuarios WHERE email = $1", [req.params.email],
       (err, result) => {
         if (err) {
-          return console.error("Erro ao executar a qry de SELECT id", err);
+          return console.error("Erro ao executar a qry de SELECT email", err);
         }
         res.send(result.rows);
-        //console.log(result);
       }
     );
   } catch (error) {
@@ -77,7 +76,6 @@ app.delete("/usuarios/:id", (req, res) => {
             res.status(200).json({ info: `Registro excluído. Código: ${req.params.id}` });
           }
         }
-        console.log(result);
       }
     );
   } catch (error) {
@@ -88,17 +86,14 @@ app.delete("/usuarios/:id", (req, res) => {
 app.post("/usuarios", (req, res) => {
   try {
     console.log("Alguém enviou um post com os dados:", req.body);
-    const { nome, email, altura, peso } = req.body;
+    const { nome, email, telefone } = req.body;
     client.query(
-      "INSERT INTO Usuarios (nome, email, altura, peso) VALUES ($1, $2, $3, $4) RETURNING * ", [nome, email, altura, peso],
+      "INSERT INTO Usuarios (nome, email, telefone) VALUES ($1, $2, $3) RETURNING * ", [nome, email, telefone],
       (err, result) => {
         if (err) {
           return console.error("Erro ao executar a qry de INSERT", err);
         }
-        const { id } = result.rows[0];
-        res.setHeader("id", `${id}`);
         res.status(201).json(result.rows[0]);
-        console.log(result);
       }
     );
   } catch (erro) {
@@ -110,55 +105,21 @@ app.put("/usuarios/:id", (req, res) => {
   try {
     console.log("Alguém enviou um update com os dados:", req.body);
     const id = req.params.id;
-    const { nome, email, altura, peso } = req.body;
+    const { nome, email, telefone } = req.body;
     client.query(
-      "UPDATE Usuarios SET nome=$1, email=$2, altura=$3, peso=$4 WHERE id =$5 ",
-      [nome, email, altura, peso, id],
+      "UPDATE Usuarios SET nome=$1, email=$2, telefone=$3 WHERE id =$4 ",
+      [nome, email, telefone, id],
       (err, result) => {
         if (err) {
           return console.error("Erro ao executar a qry de UPDATE", err);
         } else {
           res.setHeader("id", id);
           res.status(202).json({ "identificador": id });
-          console.log(result);
         }
       }
     );
   } catch (erro) {
     console.error(erro);
-  }
-});
-
-app.post('/questionario', (req, res) => {
-  try {
-    console.log("Alguém enviou um post com os dados:", req.body);
-    const { q1, q2, q3, q4, q5 } = req.body;
-    client.query(
-      "INSERT INTO questionario (q1, q2, q3, q4, q5) VALUES ($1, $2, $3, $4, $5) RETURNING * ", [q1, q2, q3, q4, q5],
-      (err, result) => {
-        if (err) {
-          return console.error("Erro ao executar a qry de INSERT no questionario", err);
-        }
-        res.status(201).json(result.rows[0]);
-        console.log(result);
-      }
-    );
-  } catch (erro) {
-    console.error(erro);
-  }
-})
-
-app.get("/questionario", (req, res) => {
-  try {
-    client.query("SELECT * FROM questionario", function (err, result) {
-      if (err) {
-        return console.error("Erro ao executar a qry de SELECT", err);
-      }
-      res.send(result.rows);
-      console.log("Rota: get questionario");
-    });
-  } catch (error) {
-    console.log(error);
   }
 });
 
@@ -166,4 +127,4 @@ app.listen(config.port, () =>
   console.log("Servidor funcionando na porta " + config.port)
 );
 
-module.exports = app; 
+module.exports = app;
